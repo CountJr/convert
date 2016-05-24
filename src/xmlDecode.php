f79c2102fa43abf99e09077f6bb9c1f351502651
CountJr <countjr@gmail.com>
2016-05-24 22:30:07 +0200

    changed to switch

--- src/xmlDecode.php
+++ src/xmlDecode.php
@@ -1,7 +1,7 @@
 <?php
 namespace Converter\Xml\Decode;
 
-function decode($text) 
+function decode($text)
 {
     $xml   = simplexml_load_string($text, 'SimpleXMLElement', LIBXML_NOCDATA);
     $array = json_decode(json_encode($xml), true);
