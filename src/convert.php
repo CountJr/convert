2a4152e817ac7241a4538e8979321f54bea1c138
CountJr <countjr@gmail.com>
2016-05-24 19:59:56 +0200

    travis

--- src/convert.php
+++ src/convert.php
@@ -5,7 +5,7 @@ function convert($inputFile, $inputFormat, $outputFile, $outputFormat)
 {
     $decodeFile = __DIR__ . DIRECTORY_SEPARATOR . 'decoders' . DIRECTORY_SEPARATOR . $inputFormat . 'Decode.php';
     $encodeFile = __DIR__ . DIRECTORY_SEPARATOR . 'encoders' . DIRECTORY_SEPARATOR . $outputFormat . 'Encode.php';
-    if(!file_exists($decodeFile) || !file_exists($encodeFile)) {
+    if (!file_exists($decodeFile) || !file_exists($encodeFile)) {
         return false;
     } else {
         $decodeFunc = require_once $decodeFile;
