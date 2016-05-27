25ec0596e72a689132df05d1cbddbdb53077a739
CountJr <countjr@gmail.com>
2016-05-27 11:51:10 +0200

    second attempt

--- src/coders/xml.php
+++ src/coders/xml.php
@@ -35,15 +35,9 @@ function encode(array $arr)
     $array_to_xml = function ($data, &$xml_data) use (&$array_to_xml) {
         foreach ($data as $key => $value) {
             if (is_array($value)) {
-                if (is_numeric($key)) {
-                    $key = 'item'.$key; //dealing with <0/>..<n/> issues
-                }
                 $subnode = $xml_data->addChild($key);
                 $array_to_xml($value, $subnode);
             } else {
-                if (is_numeric($key)) {
-                    $key = 'item'.$key; //dealing with <0/>..<n/> issues
-                }
                 $xml_data->addChild("$key", $value);
             }
         }
