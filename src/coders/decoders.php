6597fb4a4f061266973e57f09969b1ff382114e7
CountJr <countjr@gmail.com>
2016-05-28 21:55:30 +0200

    encoders/decoders refactor

--- src/coders/decoders.php
+++ src/coders/decoders.php
@@ -7,54 +7,57 @@ use Monad\Either;
 
 function decoders()
 {
-    return [
-        /**
-         * @param string $text
-         * @return \Monad\Either
-         */
-        'json' => function (string $text) {
-            
-            $return = json_decode($text, true);
-            
-            return !json_last_error()
-                ? Either\right($return)
-                : Either\left('incorect input file' . PHP_EOL);
-        },
+    
+    $decoders = [];
 
-        /**
-         * @param string $text
-         * @return \Monad\Either
-         */
-        'xml'  => function (string $text) {
-            
-            libxml_use_internal_errors(true);
-            $xml = simplexml_load_string($text, 'SimpleXMLElement', LIBXML_NOCDATA);
-            if (libxml_get_errors()) {
-                return Either\left('incorect input file' . PHP_EOL);
-            }
-            
-            $array = json_decode(json_encode($xml), true);
-            
-            foreach ($array as $key => $value) {
-                if ((string)(int)$value === $value) {
-                    $array[$key] = (int)$value;
-                }
-            }
-            
-            return Either\right($array);
-        },
+    /**
+     * @param string $text
+     * @return \Monad\Either
+     */
+    $decoders['json'] = function (string $text) {
+        
+        $return = json_decode($text, true);
+        
+        return !json_last_error()
+            ? Either\right($return)
+            : Either\left('incorect input file' . PHP_EOL);
+    };
 
-        /**
-         * @param string $text
-         * @return \Monad\Either
-         */
-        'yml'  => function (string $text) {
-            
-            return \Functional\tryCatch(function ($text) {
-                return Either\right(Yaml::parse($text));
-            }, function (\Exception $e) {
-                return Either\left('incorect input file' . PHP_EOL);
-            }, $text);
-        },
-    ];
+    /**
+     * @param string $text
+     * @return \Monad\Either
+     */
+    $decoders['xml']  = function (string $text) {
+        
+        libxml_use_internal_errors(true);
+        $xml = simplexml_load_string($text, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if (libxml_get_errors()) {
+            return Either\left('incorect input file' . PHP_EOL);
+        }
+        
+        $array = json_decode(json_encode($xml), true);
+        
+        foreach ($array as $key => $value) {
+            if ((string)(int)$value === $value) {
+                $array[$key] = (int)$value;
+            }
+        }
+        
+        return Either\right($array);
+    };
+
+    /**
+     * @param string $text
+     * @return \Monad\Either
+     */
+    $decoders['yml']  = function (string $text) {
+        
+        return \Functional\tryCatch(function ($text) {
+            return Either\right(Yaml::parse($text));
+        }, function (\Exception $e) {
+            return Either\left('incorect input file' . PHP_EOL);
+        }, $text);
+    };
+    
+    return $decoders;
 }
--- src/coders/encoders.php
+++ src/coders/encoders.php
@@ -7,53 +7,56 @@ use Monad\Either;
 
 function encoders()
 {
-    return [
-        /**
-         * @param array $arr
-         * @return \Monad\Either
-         */
-        'json' => function (array $arr) {
+    
+    $encoders = [];
+    
+    /**
+     * @param array $arr
+     * @return Either\Right
+     */
+    $encoders['json'] = function (array $arr) {
 
-            return Either\right(json_encode($arr, JSON_UNESCAPED_UNICODE));
-        },
+        return Either\right(json_encode($arr, JSON_UNESCAPED_UNICODE));
+    };
 
-        /**
-         * @param array $arr
-         * @return \Monad\Either
-         */
-        'xml' => function (array $arr) {
+    /**
+     * @param array $arr
+     * @return \Monad\Either
+     */
+    $encoders['xml'] = function (array $arr) {
 
-            $arrayToXml = function ($data, &$xmlData) use (&$arrayToXml) {
-                foreach ($data as $key => $value) {
-                    if (is_array($value)) {
-                        $subnode = $xmlData->addChild($key);
-                        $arrayToXml($value, $subnode);
-                    } else {
-                        $xmlData->addChild("$key", $value);
-                    }
+        $arrayToXml = function ($data, &$xmlData) use (&$arrayToXml) {
+            foreach ($data as $key => $value) {
+                if (is_array($value)) {
+                    $subnode = $xmlData->addChild($key);
+                    $arrayToXml($value, $subnode);
+                } else {
+                    $xmlData->addChild("$key", $value);
                 }
-            };
+            }
+        };
+    
+        $xmlData = new \SimpleXMLElement(
+            '<?xml version="1.0" encoding="UTF-8"?><data></data>'
+        );
+    
+        $arrayToXml($arr, $xmlData);
+    
+        return  Either\right($xmlData->asXML());
+    };
 
-            $xmlData = new \SimpleXMLElement(
-                '<?xml version="1.0" encoding="UTF-8"?><data></data>'
-            );
+    /**
+     * @param array $arr
+     * @return \Monad\Either
+     */
+    $encoders['yml'] = function (array $arr) {
 
-            $arrayToXml($arr, $xmlData);
-
-            return  Either\right($xmlData->asXML());
-        },
-
-        /**
-         * @param array $arr
-         * @return \Monad\Either
-         */
-        'yml' => function (array $arr) {
-
-            return \Functional\tryCatch(function ($arr) {
-                return Either\right(Yaml::dump($arr, 2, 2));
-            }, function (\Exception $e) {
-                return Either\left('incorect data' . PHP_EOL);
-            }, $arr);
-        },
-    ];
+        return \Functional\tryCatch(function ($arr) {
+            return Either\right(Yaml::dump($arr, 2, 2));
+        }, function (\Exception $e) {
+            return Either\left('incorect data' . PHP_EOL);
+        }, $arr);
+    };
+    
+    return $encoders;
 }
